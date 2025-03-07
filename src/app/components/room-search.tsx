b27b2db627ed6236d4237f1d984d12850d8501d7
Keisuke <mtcs_drsp@icloud.com>
2025-03-07 01:57:20 +0000

    room-search.tsx作成

---
+++ src/app/components/room-search.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+interface RoomSearchProps {
+    onSearch: (query: string) => void
+}
+
+export default function RoomSearch({onSearch}: RoomSearchProps) {
+    return(
+        <div className="relative">
+            <input 
+                type="search"
+                placeholder="部屋番号で検索"
+                className="w-full rounded-full
+                px-4 py-2 pr-10 border border-gray-300
+                focus:outline-none focus:ring-2 focus:ring-sky-300"
+                onChange={(e) => onSearch(e.target.value)}
+                />
+            <span className="absolute right-3 top-1/2 -translate-y-1/2">🔍</span>
+        </div>
+    )
+}
