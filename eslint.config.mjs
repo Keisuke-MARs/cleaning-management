54a2a52b0490e41e101eff63ea4d03ec2a110f08
Keisuke-MARs <mtcs_drsp@icloud.com>
2025-04-15 10:17:57 +0900

    許容

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇ここを追加！
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
