f3e48be72fe8401d8c5de6e8137075894e2dc72e
shionn2015 <bell.stitch.0527.shionn@gmail.com>
2025-05-12 02:54:10 +0000

    色変更完了

--- src/app/components/date-display.tsx
+++ src/app/components/date-display.tsx
@@ -13,11 +13,11 @@ const weekdayColors = {
 }
 
 export default function DateDisplay() {
-    const [date, setDate] = useState<Date>(new Date("2025-05-12"))
+    const [date, setDate] = useState<Date>(new Date())
 
-    // useEffect(() => {
-    //     setDate(new Date())
-    // }, [])
+    useEffect(() => {
+        setDate(new Date())
+    }, [])
 
     const weekdays = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
     const formattedDate = `${date.getFullYear()}年${String(date.getMonth() + 1).padStart(2, "0")}月${String(date.getDate()).padStart(2, "0")}日`
